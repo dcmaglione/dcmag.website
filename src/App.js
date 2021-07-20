1ebd2c0a06f3e2772cbbb533896023de8b87dc37
Dominic Maglione <dcmaglione@gmail.com>
2021-07-19 23:18:48 -0400

    Fixed import error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import About from './components/About';
-import Contact from './components/Contact';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
